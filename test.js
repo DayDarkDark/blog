a1058aa89139a04187c5ac83ca37aacc24a2c114
daydarkdark <daydarkdark@daydarkdarkdeMac-mini.local>
2021-01-15 16:45:39 +0800

    [fix] 输入内容为空的时候

--- test.js
+++ test.js
@@ -1,19 +1,39 @@
-var longestCommonPrefix = function(strs) {
-  if(strs.length == []){
-    return ""
-  }
-  // strs.sort((a, b) => a.length - b.length)
-  let std = strs[0]
-  let pre = ''
-  for (let i = 0 ; i < std.length; i ++) {
-    if (strs.every(a => a[i] == std[i])) {
-      pre += std[i]
-    } else {
-      break
-    }
-  }
-  return pre
-}
-console.log(longestCommonPrefix(["abab","aba","abc"]))
 
-// console.log(longestCommonPrefix(["flower","flow","flight"]))
+// class People {
+//   get name() {
+//     return this.nn
+//   }
+//   constructor(name) {
+//     this.nn = name
+//   }
+  
+// }
+
+// Object.defineProperty(People, 'name', {
+//   writable: false,
+//   set:
+// }) 
+
+class People {
+  constructor(name) {
+    this._name = name;
+  }
+
+  set name(name) {
+    console.log("不允许");
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  setName(name) {
+    this._name = name;
+  }
+}
+
+
+let men =  new People('小红')
+men.name = '小白' // 提示不可以修改
+console.log(men)
+// men.setName('小白') // 可修改

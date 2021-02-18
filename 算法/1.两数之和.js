f8858a246f32a6f98aaa48a88b19d590983930c3
daydarkdark <daydarkdark@gmail.com>
2021-02-18 22:06:29 +0800

    2021021820210218

--- 算法/1.两数之和.js
+++ 算法/1.两数之和.js
@@ -1,8 +1,8 @@
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     let index = nums.indexOf(target - nums[i]) 
-    if ((index !== -1 && index !== i)) {
-       return [ i, index ]
+    if (index !== -1 && index !== i) {
+      return [ i, index ]
     }
   }
-};
+}
